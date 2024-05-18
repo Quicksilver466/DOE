348caaf74b3f2e2d08407275a0d30a4bfac4f170
Quicksilver466 <shaunakketkar@gmail.com>
2024-05-18 19:09:47 +0530

    Updated code for transforming Linear module

--- fx_test.py
+++ fx_test.py
@@ -1,20 +1,22 @@
 import torch
+from torch import nn
 import torch.fx as fx
-from torch.fx import symbolic_trace
+from torch.fx import symbolic_trace, replace_pattern
 import time
-import re
 
-def transform_test(module: torch.nn.Module, replacement_module: torch.nn.Module) -> torch.nn.Module:
-    nn_graph: fx.GraphModule = symbolic_trace(module)
-
-    for node in nn_graph.graph.nodes:
-        if(node.op == "call_module" and re.search(r"linear", node.target)): node.target = replacement_module
-
-class LinLayers(torch.nn.Module):
+class Pattern(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.linear1 = torch.nn.Linear(10, 5)
-        self.linear2 = torch.nn.Linear(5, 2)
+        self.linear = nn.Linear(10, 5)
+
+    def forward(self, x: torch.Tensor):
+        return self.linear(x)
+
+class LinLayers(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(10, 5)
+        self.linear2 = nn.Linear(5, 2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         output = self.linear1(x)
@@ -22,17 +24,17 @@ class LinLayers(torch.nn.Module):
 
         return output
 
-class SingLinLayer(torch.nn.Module):
+class SingLinLayer(nn.Module):
     def __init__(self, in_feats: int, out_feats: int) -> None:
         super().__init__()
-        self.linear = torch.nn.Linear(in_feats, out_feats)
+        self.linear = nn.Linear(in_feats, out_feats)
         self.state = None
 
     def forward(self, x: torch.Tensor):
         self.state = x.sum(dim=0)
         return self.linear(x)
 
-class NN_mod(torch.nn.Module):
+class NN_mod(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.linear1 = SingLinLayer(10, 5)
@@ -46,24 +48,26 @@ class NN_mod(torch.nn.Module):
 
 tic = time.time()
 
-nn = LinLayers()
-my_lin = SingLinLayer(10, 5)
+lin_lay = LinLayers()
+sing_lin = SingLinLayer(10, 5)
 nn_mod = NN_mod()
 
-nn_graph: fx.GraphModule = symbolic_trace(nn)
-lin_graph: fx.GraphModule = symbolic_trace(my_lin)
+nn_graph: fx.GraphModule = symbolic_trace(lin_lay)
+lin_graph: fx.GraphModule = symbolic_trace(sing_lin)
 mod_graph: fx.GraphModule = symbolic_trace(nn_mod)
-
-
-
-#for node in nn_graph.graph.nodes:
-#    print(node.args)
 
 nn_graph.graph.print_tabular()
 print("\n")
 lin_graph.graph.print_tabular()
 print("\n")
 mod_graph.graph.print_tabular()
+print("\n")
+
+symbolic_trace(Pattern()).graph.print_tabular()
+print("\n")
+
+replace_pattern(nn_graph, Pattern(), sing_lin)
+nn_graph.graph.print_tabular()
 
 toc = time.time()
 print(f"\nIt took {toc - tic} seconds to execute above code")
