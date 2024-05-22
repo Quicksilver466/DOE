43ee2506b9fe3293ddf7bf25b492360d6ddc2824
Quicksilver466 <shaunakketkar@gmail.com>
2024-05-23 01:07:34 +0530

    Refactored gating and separated it

--- src/models/experts.py
+++ src/models/experts.py
@@ -1,12 +1,13 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
 from dataclasses import dataclass
 
 @dataclass
 class TempConfig:
     intermediate_size: int
     hidden_size: int
-    num_local_experts: int = 0
+    num_local_experts: int = 1
+    threshold: float = 0.5
 
 class MLP(nn.Module):
     def __init__(self, config: TempConfig) -> None:
@@ -20,7 +21,7 @@ class MLP(nn.Module):
 
         self.act_fn = nn.ReLU()
 
-    def forward(self, hidden_states):
+    def forward(self, hidden_states: Tensor) -> Tensor:
         current_hidden_states = self.act_fn(self.w1(hidden_states)) * self.w3(hidden_states)
         current_hidden_states = self.w2(current_hidden_states)
         return current_hidden_states
@@ -28,24 +29,37 @@ class MLP(nn.Module):
 class TaskExperts(nn.Module):
     def __init__(self, config: TempConfig) -> None:
         super().__init__()
+        self.num_experts = config.num_local_experts
+        self.experts = nn.ModuleList([MLP(config) for _ in range(self.num_experts)])
+
+    def forward(self, hidden_states: Tensor, expert_indices: Tensor) -> Tensor:
+        pass
+
+class Gate(nn.Module):
+    def __init__(self, config: TempConfig) -> None:
+        super().__init__()
         self.hidden_dim = config.hidden_size
         self.num_experts = config.num_local_experts
-
-        self.gates = nn.Linear(self.hidden_dim, self.num_experts, bias=False)
-        self.experts = nn.ModuleList([MLP(config) for _ in range(self.num_experts)])
+        self.gate = nn.Linear(self.hidden_dim, self.num_experts, bias=False)
         self.sig_func = nn.Sigmoid()
-        self.expert_indices = []
-        self.threshold = 0.5
+        self.threshold = config.threshold
 
-    def forward(self, hidden_states: torch.Tensor, decide_experts=False) -> torch.Tensor:
-        if decide_experts: self.reset_indices(hidden_states)
-        for index in self.expert_indices:
-            output = self.experts[index](hidden_states)
+    def forward(self, cls_hidden_states: Tensor) -> Tensor:
+        gating_logits = self.gate(cls_hidden_states)
+        print(gating_logits)
+        gating_output = self.sig_func(gating_logits)
+        gating_output = gating_output > self.threshold
+        gating_output = gating_output.float()
+        return gating_output
+    
+config = TempConfig(intermediate_size=5, hidden_size=5, num_local_experts=3)
+samples = torch.tensor(
+    [
+        [-2, 0.2, 4, 0.3, 0.5],
+        [0, -0.2, -4, 2, 0.5]
+    ]
+)
 
-        return output
-
-    def reset_indices(self, hidden_states: torch.Tensor) -> None:
-        cls_state = hidden_states[..., 0, :]
-        gating_logits = self.gates(cls_state)
-        gating_logits = self.sig_func(gating_logits)
-        pass
+gate = Gate(config)
+output = gate(samples)
+print(output)
