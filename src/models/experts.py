ea00db04753cc2b20277d794733a92567a48d144
Quicksilver466 <shaunakketkar@gmail.com>
2024-05-24 17:30:45 +0530

    Finalized Gate operations and batching for experts

--- src/models/experts.py
+++ src/models/experts.py
@@ -9,7 +9,22 @@ class TempConfig:
     num_local_experts: int = 1
     threshold: float = 0.5
 
-class MLP(nn.Module):
+class Gate(nn.Module):
+    def __init__(self, config: TempConfig) -> None:
+        super().__init__()
+        self.hidden_dim = config.hidden_size
+        self.num_experts = config.num_local_experts
+        self.gate = nn.Linear(self.hidden_dim, self.num_experts, bias=False)
+        self.sig_func = nn.Sigmoid()
+        self.threshold = config.threshold
+
+    def forward(self, cls_hidden_states: Tensor) -> Tensor:
+        gating_logits = self.gate(cls_hidden_states)
+        gating_output = self.sig_func(gating_logits)
+        gating_output = torch.where(gating_output > self.threshold, 1, 0)
+        return gating_output
+
+class Expert(nn.Module):
     def __init__(self, config: TempConfig) -> None:
         super().__init__()
         self.ffn_dim = config.intermediate_size
@@ -26,40 +41,18 @@ class MLP(nn.Module):
         current_hidden_states = self.w2(current_hidden_states)
         return current_hidden_states
 
-class TaskExperts(nn.Module):
+class ExpertsModule(nn.Module):
     def __init__(self, config: TempConfig) -> None:
         super().__init__()
         self.num_experts = config.num_local_experts
-        self.experts = nn.ModuleList([MLP(config) for _ in range(self.num_experts)])
+        self.experts = nn.ModuleList([Expert(config) for _ in range(self.num_experts)])
 
     def forward(self, hidden_states: Tensor, expert_indices: Tensor) -> Tensor:
-        pass
+        outputs = []
+        expert_indices = expert_indices.permute(1, 0)
+        for i, expert_index_tensor in enumerate(expert_indices):
+            ones_indices = torch.nonzero(expert_index_tensor)
+            expert_batch = torch.index_select(hidden_states, 0, ones_indices.view(-1))
+            outputs.append(self.experts[i](expert_batch))
 
-class Gate(nn.Module):
-    def __init__(self, config: TempConfig) -> None:
-        super().__init__()
-        self.hidden_dim = config.hidden_size
-        self.num_experts = config.num_local_experts
-        self.gate = nn.Linear(self.hidden_dim, self.num_experts, bias=False)
-        self.sig_func = nn.Sigmoid()
-        self.threshold = config.threshold
-
-    def forward(self, cls_hidden_states: Tensor) -> Tensor:
-        gating_logits = self.gate(cls_hidden_states)
-        print(gating_logits)
-        gating_output = self.sig_func(gating_logits)
-        gating_output = gating_output > self.threshold
-        gating_output = gating_output.float()
-        return gating_output
-    
-config = TempConfig(intermediate_size=5, hidden_size=5, num_local_experts=3)
-samples = torch.tensor(
-    [
-        [-2, 0.2, 4, 0.3, 0.5],
-        [0, -0.2, -4, 2, 0.5]
-    ]
-)
-
-gate = Gate(config)
-output = gate(samples)
-print(output)
+        return outputs
