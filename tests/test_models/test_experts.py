10a33b932c6f13e36a1517c68f469d71ee9d59ca
Quicksilver466 <shaunakketkar@gmail.com>
2024-05-27 23:59:06 +0530

    Added setup for testing the gating module

--- tests/test_models/test_experts.py
+++ tests/test_models/test_experts.py
@@ -1,1 +1,20 @@
 import unittest
+from src.models.experts import TempConfig, Gate, Expert, ExpertsModule
+import torch
+from torch.fx import GraphModule, symbolic_trace, replace_pattern
+
+def replace_linear(module: GraphModule) -> GraphModule:
+    pass # Replace all linear opertations with identity operations
+
+class TestExperts(unittest.TestCase):
+    def setUp(self) -> None:
+        self.intermediate_size = 5
+        self.hidden_size = 5
+        self.config = TempConfig(intermediate_size=self.intermediate_size, hidden_size=self.hidden_size, num_local_experts=self.hidden_size)
+        self.gate = Gate(config=self.config)
+    
+    def test_gating():
+        pass
+
+    def test_experts_module():
+        pass
