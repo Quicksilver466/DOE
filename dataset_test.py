4516ba73df57f078730e22ac7a56ec6ee2cfeb75
Quicksilver466 <shaunakketkar@gmail.com>
2024-06-04 12:23:54 +0530

    Added transformations for phi-3 and token counter function for datasets map

--- dataset_test.py
+++ dataset_test.py
@@ -1,28 +1,57 @@
 from datasets import load_dataset
 from datasets import disable_caching
 from src.datasets.datasets_transform import code_feedback_transform, maths_transform, medic_transform, mult_transform
+from src.datasets.datasets_transform import code_feedback_transform_phi3, maths_transform_phi3, medic_transform_phi3, mult_transform_phi3
 
 disable_caching()
 
-DATASETS = {
-    "m-a-p/CodeFeedback-Filtered-Instruction": "/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction",
-    "TIGER-Lab/MathInstruct": "/data/Datasets-LLMS/MathInstruct",
-    "lavita/ChatDoctor-HealthCareMagic-100k": "/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k",
-    "fnlp/moss-002-sft-data": "/data/Datasets-LLMS/moss-002-sft-data"
-}
+def std_transformation():
+    datasets = {
+        "m-a-p/CodeFeedback-Filtered-Instruction": "/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction",
+        "TIGER-Lab/MathInstruct": "/data/Datasets-LLMS/MathInstruct",
+        "lavita/ChatDoctor-HealthCareMagic-100k": "/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k",
+        "fnlp/moss-002-sft-data": "/data/Datasets-LLMS/moss-002-sft-data"
+    }
 
-dataset = load_dataset(DATASETS["m-a-p/CodeFeedback-Filtered-Instruction"], split="train")
-result = dataset.map(code_feedback_transform, remove_columns=["query", "answer", "resource", "lang"])
-result.save_to_disk("/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction-Transformed")
+    dataset = load_dataset(datasets["m-a-p/CodeFeedback-Filtered-Instruction"], split="train")
+    result = dataset.map(code_feedback_transform, remove_columns=["query", "answer", "resource", "lang"])
+    result.save_to_disk("/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction-Transformed")
 
-dataset = load_dataset(DATASETS["TIGER-Lab/MathInstruct"], split="train")
-result = dataset.map(maths_transform, remove_columns=["source", "output", "instruction"])
-result.save_to_disk("/data/Datasets-LLMS/MathInstruct-Transformed")
+    #dataset = load_dataset(datasets["TIGER-Lab/MathInstruct"], split="train")
+    #result = dataset.map(maths_transform, remove_columns=["source", "output", "instruction"])
+    #result.save_to_disk("/data/Datasets-LLMS/MathInstruct-Transformed")
+#
+    #dataset = load_dataset(datasets["lavita/ChatDoctor-HealthCareMagic-100k"], split="train")
+    #result = dataset.map(medic_transform, remove_columns=["input", "output", "instruction"])
+    #result.save_to_disk("/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k-Transformed")
+#
+    #dataset = load_dataset(datasets["fnlp/moss-002-sft-data"], split="train")
+    #result = dataset.map(mult_transform, remove_columns=["num_turns", "plain_text", "prefix", "id"])
+    #result.save_to_disk("/data/Datasets-LLMS/moss-002-sft-data-Transformed")
 
-dataset = load_dataset(DATASETS["lavita/ChatDoctor-HealthCareMagic-100k"], split="train")
-result = dataset.map(medic_transform, remove_columns=["input", "output", "instruction"])
-result.save_to_disk("/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k-Transformed")
+def phi3_transformation():
+    datasets = {
+        "m-a-p/CodeFeedback-Filtered-Instruction": "/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction",
+        "TIGER-Lab/MathInstruct": "/data/Datasets-LLMS/MathInstruct",
+        "lavita/ChatDoctor-HealthCareMagic-100k": "/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k",
+        "fnlp/moss-002-sft-data": "/data/Datasets-LLMS/moss-002-sft-data"
+    }
 
-dataset = load_dataset(DATASETS["fnlp/moss-002-sft-data"], split="train")
-result = dataset.map(mult_transform, remove_columns=["num_turns", "plain_text", "prefix", "id"])
-result.save_to_disk("/data/Datasets-LLMS/moss-002-sft-data-Transformed")
+    dataset = load_dataset(datasets["m-a-p/CodeFeedback-Filtered-Instruction"], split="train")
+    result = dataset.map(code_feedback_transform_phi3, remove_columns=["query", "answer", "resource", "lang"])
+    result.save_to_disk("/data/Datasets-LLMS/CodeFeedback-Filtered-Instruction-Transformed-phi3")
+
+    #dataset = load_dataset(datasets["TIGER-Lab/MathInstruct"], split="train")
+    #result = dataset.map(maths_transform_phi3, remove_columns=["source", "output", "instruction"])
+    #result.save_to_disk("/data/Datasets-LLMS/MathInstruct-Transformed-phi3")
+#
+    #dataset = load_dataset(datasets["lavita/ChatDoctor-HealthCareMagic-100k"], split="train")
+    #result = dataset.map(medic_transform_phi3, remove_columns=["input", "output", "instruction"])
+    #result.save_to_disk("/data/Datasets-LLMS/ChatDoctor-HealthCareMagic-100k-Transformed-phi3")
+#
+    #dataset = load_dataset(datasets["fnlp/moss-002-sft-data"], split="train")
+    #result = dataset.map(mult_transform_phi3, remove_columns=["num_turns", "plain_text", "prefix", "id"])
+    #result.save_to_disk("/data/Datasets-LLMS/moss-002-sft-data-Transformed-phi3")
+
+std_transformation()
+phi3_transformation()
--- src/datasets/datasets_transform.py
+++ src/datasets/datasets_transform.py
@@ -84,3 +84,74 @@ def mult_transform(example):
 
     assert(num_turns == example["num_turns"])
     return {"text": messages}
+
+
+def code_feedback_transform_phi3(example):
+    sys_msg = SYS_PROMPT_CODING % example["lang"]
+    messages = [
+        {
+            "role": "user",
+            "content": f"{sys_msg}\n" + example["query"]
+        },
+        {
+            "role": "assistant",
+            "content": example["answer"]
+        }
+    ]
+
+    return {"text": messages}
+
+def maths_transform_phi3(example):
+    sys_msg = SYS_PROMPT_MATHS
+    messages = [
+        {
+            "role": "user",
+            "content": f"{sys_msg}\n" + example["instruction"]
+        },
+        {
+            "role": "assistant",
+            "content": example["output"]
+        }
+    ]
+
+    return {"text": messages}
+
+def medic_transform_phi3(example):
+    sys_msg = SYS_PROMPT_MEDIC
+    messages = [
+        {
+            "role": "user",
+            "content": f"{sys_msg}\n" + example["input"]
+        },
+        {
+            "role": "assistant",
+            "content": example["output"]
+        }
+    ]
+
+    return {"text": messages}
+
+def mult_transform_phi3(example):
+    convos = re.split(r"<eoh>|<eoa>", example["plain_text"])
+    messages = []
+
+    num_turns = 0
+
+    for convo in convos:
+        if not convo: continue
+
+        if(convo.strip().startswith("[Human]:")):
+            messages.append({
+                "role": "user",
+                "content": convo.replace("[Human]:", "").strip()
+            })
+            num_turns += 1
+        
+        if(convo.strip().startswith("[MOSS]:")):
+            messages.append({
+                "role": "assistant",
+                "content": convo.replace("[MOSS]:", "").strip()
+            })
+
+    assert(num_turns == example["num_turns"])
+    return {"text": messages}
