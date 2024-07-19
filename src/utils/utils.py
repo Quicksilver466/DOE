cf922cecbde6fc4f3949cd221b3e6ad603ea369c
Quicksilver466 <shaunakketkar@gmail.com>
2024-07-20 00:56:28 +0530

    Converted config base path from global variable(in file) to default argument

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,7 +3,6 @@ from typing import Literal
 import os
 import logging
 
-CONFIGS_BASE_PATH = "./src/configs"
 CONFIGS_FILENAMES = {
     "dataset": "dataset-configs.json",
     "model": "model-configs.json",
@@ -14,8 +13,8 @@ CONFIGS_FILENAMES = {
 
 ERROR_LOGGER = logging.getLogger("DOE-Error")
 
-def load_configs(config_type: Literal["dataset", "model", "trainer", "lora", "sft-trainer"]) -> dict:
-    config_path = os.path.join(CONFIGS_BASE_PATH, CONFIGS_FILENAMES.get(config_type))
+def load_configs(config_type: Literal["dataset", "model", "trainer", "lora", "sft-trainer"], configs_base_path="./src/configs") -> dict:
+    config_path = os.path.join(configs_base_path, CONFIGS_FILENAMES.get(config_type))
     dataset_configs = json.load(open(config_path, "r"))
     return dataset_configs
 
@@ -24,7 +23,12 @@ class SetEncoder(json.JSONEncoder):
         if isinstance(obj, set): return list(obj)
         return json.JSONEncoder.default(self, obj)
     
-def save_config(config: dict, config_type: Literal["dataset", "model", "trainer", "lora"], use_set_encoder=False) -> bool:
+def save_config(
+        config: dict, 
+        config_type: Literal["dataset", "model", "trainer", "lora", "sft-trainer"], 
+        configs_base_path="./src/configs", 
+        use_set_encoder=False
+) -> bool:
     """Save any config to json pre-configured files
 
     Args:
@@ -37,7 +41,7 @@ def save_config(config: dict, config_type: Literal["dataset", "model", "trainer"
         bool: Returns whether saving operation was successful or not. Incase it isn't successful, logger will log appropriate exception
     """
     try:
-        config_path = os.path.join(CONFIGS_BASE_PATH, CONFIGS_FILENAMES.get(config_type))
+        config_path = os.path.join(configs_base_path, CONFIGS_FILENAMES.get(config_type))
         cls = SetEncoder if use_set_encoder else None
         json.dump(config, open(config_path, "w"), cls=cls)
         return True
