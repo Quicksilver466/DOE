df24849beaa2170e5056b78b9bf3d0d2253963e1
Quicksilver466 <shaunakketkar@gmail.com>
2024-07-21 23:49:58 +0530

    Added initial startup shell file for container

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+curl -o gitlfs.tar.gz -L https://github.com/git-lfs/git-lfs/releases/download/v3.5.1/git-lfs-linux-amd64-v3.5.1.tar.gz
+tar -xvzf gitlfs.tar.gz
+git lfs install
+
+makedir -p ./tmp/datasets
+cd ./tmp/datasets
+git clone https://huggingface.co/datasets/Quicksilver1/DOE-Merged-Tokenized-v1
+cd ..
+cd ..
+
+makedir -p ./tmp/models
+cd ./tmp/models
+git clone https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
+cd ..
+cd ..
+
+echo success
--- src/datasets/prepare_dataset.py
+++ src/datasets/prepare_dataset.py
@@ -1,4 +1,4 @@
 from datasets import load_from_disk
 
-def get_dataset(dataset_path="./tmp/datasets/Merged-Shuffled-phi3-tokenized"):
+def get_dataset(dataset_path="./tmp/datasets/DOE-Merged-Tokenized-v1"):
     return load_from_disk(dataset_path=dataset_path)
